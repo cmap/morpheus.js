be61fc03a4d2c83b5456de647c55e8968006eff4
jgould <jgould@broadinstitute.org>
2017-01-06 16:06:35 -0500

    icon size

--- src/ui/help_menu.js
+++ src/ui/help_menu.js
@@ -4,8 +4,7 @@ morpheus.HelpMenu = function () {
 	html.push('<button type="button" class="btn btn-default btn-xxs' +
 		' dropdown-toggle"' +
 		' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
-	html.push('<svg width="16px" height="16px"><g><rect x="0" y="0" width="16" height="7"' +
-		' style="fill:#ca0020;stroke:none"/><rect x="0" y="9" width="16" height="7" style="fill:#0571b0;stroke:none"/></g></svg>');
+	html.push('<svg width="14px" height="14px" style="inline-block;vertical-align:middle;"><g><rect x="0" y="0" width="14" height="6" style="fill:#ca0020;stroke:none"></rect><rect x="0" y="7" width="14" height="6" style="fill:#0571b0;stroke:none"></rect></g></svg>');
 	html.push(' <span class="fa fa-caret-down"></span>');
 	html.push('</button>');
 	html
