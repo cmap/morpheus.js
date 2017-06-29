6c5798f9d9b125703217d82cacb17b4de5715a65
jgould <jgould@broadinstitute.org>
2017-06-29 18:34:42 -0400

    show # matches

--- src/ui/checkbox_list.js
+++ src/ui/checkbox_list.js
@@ -77,7 +77,23 @@ morpheus.CheckBoxList = function (options) {
   var $selectAll = $checkBoxEl.find('[name=selectAll]');
   var $selectNone = $checkBoxEl.find('[name=selectNone]');
   var $cb = $checkBoxEl.find('[data-name=checkbox]');
+  var updateLabel = function () {
+    var label = [];
+    label.push('selected ');
+    label.push(morpheus.Util.intFormat(set.size()));
+    label.push(' of ');
+    label.push(morpheus.Util.intFormat(table.getAllItemCount()));
+    if (table.getFilteredItemCount() !== table.getAllItemCount()) {
+      label.push(', ');
+      label.push(morpheus.Util.intFormat(table.getFilteredItemCount()));
+      label.push(table.getFilteredItemCount() === 1 ? ' match' : ' matches');
+    }
+    $selection.html(label.join(''));
 
+  };
+  table.grid.on('filter', function (e) {
+    updateLabel();
+  });
   $cb.on('click', function (e) {
     if ($cb.hasClass('fa-square-o')) {
       var items = table.getItems(); // select all
@@ -145,7 +161,7 @@ morpheus.CheckBoxList = function (options) {
 
   this.set = set;
   this.table = table;
-  $selection.html(morpheus.Util.intFormat(set.size()) + '/' + morpheus.Util.intFormat(table.getAllItemCount()));
+  updateLabel();
 
   var priorCount = 0;
   this.table.on('checkBoxSelectionChanged', function () {
@@ -179,7 +195,7 @@ morpheus.CheckBoxList = function (options) {
       }
     }
 
-    $selection.html(morpheus.Util.intFormat(set.size()) + '/' + morpheus.Util.intFormat(table.getAllItemCount()));
+    updateLabel();
 
     _this.table.redraw();
   });
