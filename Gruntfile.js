7f1c1ecb785a0e14dcf70137b3e21be49381c210
jgould <jgould@broadinstitute.org>
2017-07-25 08:35:59 -0400

    travis ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,16 +80,29 @@ module.exports = function (grunt) {
     watch: {
       files: ['src/*.js', 'src/**/*.js'],
       tasks: ['concat:morpheus']
+    },
+    jasmine: {
+      morpheus: {
+        options: {
+          specs: 'jasmine/spec/*.js',
+          helpers: ['jasmine/matchers/*.js'],
+          vendor: [
+            'js/morpheus-external-latest.min.js', 'js/morpheus-latest.min.js', 'jasmine/jasmine-helper.js'
+          ]
+        }
+      }
     }
   });
 
-  // rebuild js and css:
-  // grunt concat:morpheus concat:extJs uglify concat:extJsAll
   grunt.registerTask('default', 'watch');
+  grunt.registerTask('travis', [
+    'concat', 'uglify', 'jasmine'
+  ]);
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 };
