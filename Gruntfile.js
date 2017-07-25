ccb52b5c3aeb6a65a7e512019b525e4c388bb244
jgould <jgould@broadinstitute.org>
2017-07-25 08:58:33 -0400

    dependency update

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,6 +103,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
 };
