6cf724b8f0658c05ed7c5bf43ddeaac60cd54cb8
jgould <jgould@broadinstitute.org>
2017-07-27 09:42:20 -0400

    use PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -66,14 +66,8 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
-    customLaunchers: {
-      MyChrome: {
-        base: 'Chrome',
-        flags: ['--disable-hang-monitor']
-      }
-    },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
